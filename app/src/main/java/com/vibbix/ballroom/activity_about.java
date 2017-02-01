0482fb6f3106fb1170f957755ac6469a4ef6ac86
Mark Beznos <mark.vibbix@gmail.com>
2017-02-01 14:37:35 -0500

    -Added links to about menu -added nightmode spinner to about -began SharedPreferences work in main activity -added links to main menu -added night mode colors -fixed some spelling mistakes

--- app/src/main/java/com/vibbix/ballroom/activity_about.java
+++ app/src/main/java/com/vibbix/ballroom/activity_about.java
@@ -1,39 +1,139 @@
 package com.vibbix.ballroom;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import butterknife.BindArray;
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnItemSelected;
 
 /**
  * The about menu activity
  */
 public class activity_about extends AppCompatActivity {
+    //region binded views
     @BindView(R.id.versionnumber)
-        TextView version;
+    TextView version;
     @BindView(R.id.toolbar)
-        Toolbar toolbar;
+    Toolbar toolbar;
+    @BindView(R.id.spinner_nightmode)
+    AppCompatSpinner nightmode;
     @BindString(R.string.versionstr)
     String versionfmt;
-        /** Called when the activity is first created. */
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_about);
-            ButterKnife.bind(this);
-            setSupportActionBar(toolbar);
-            String versionno = "ERROR";
-            try {
-                versionno = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0).versionName;
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
-            this.version.setText(String.format(versionfmt, versionno));
+    @BindString(R.string.link_finn)
+    String finnlink;
+    @BindString(R.string.link_berge)
+    String bergelink;
+    @BindArray(R.array.spinner_nightmode_items)
+    String[] nightmodeItems;
+    @BindString(R.string.prefs_nightmode)
+    String prefsNightmode;
+    @BindString(R.string.prefs_name)
+    String prefsName;
+    //endregion
 
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        try {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        } catch (NullPointerException npe) {
+            npe.printStackTrace();
         }
+        String versionno = "ERROR";
+        try {
+            versionno = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        this.version.setText(String.format(versionfmt, versionno));
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_nightmode_items, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        nightmode.setAdapter(adapter);
+        loadNightMode();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_finn:
+                Intent finnIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(this.finnlink));
+                startActivity(finnIntent);
+                return true;
+            case R.id.action_berge:
+                Intent bergeIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(this.bergelink));
+                startActivity(bergeIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnItemSelected(value = R.id.spinner_nightmode, callback = OnItemSelected.Callback.ITEM_SELECTED)
+    void selectNightMode(int position) {
+        SharedPreferences settings = getSharedPreferences(prefsName, 0);
+        SharedPreferences.Editor edit = settings.edit();
+        if (position == 0) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+            edit.putString(prefsNightmode, nightmodeItems[0]);
+        } else if (position == 1) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            edit.putString(prefsNightmode, nightmodeItems[1]);
+        } else if (position == 2) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            edit.putString(prefsNightmode, nightmodeItems[2]);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+            edit.putString(prefsNightmode, nightmodeItems[3]);
+        }
+        edit.apply();
+    }
+
+    private void loadNightMode() {
+        SharedPreferences settings = getSharedPreferences(prefsName, 0);
+        String mode = settings.getString(this.prefsNightmode, "Auto");
+        if (mode.equals("Auto"))
+            this.nightmode.setSelection(0, true);
+        else if (mode.equals("Day"))
+            this.nightmode.setSelection(1, true);
+        else if (mode.equals("Night"))
+            this.nightmode.setSelection(2, true);
+        else if (mode.equals("Follow"))
+            this.nightmode.setSelection(3, true);
+
+    }
+
+
 }
--- app/src/main/java/com/vibbix/ballroom/activity_main.java
+++ app/src/main/java/com/vibbix/ballroom/activity_main.java
@@ -1,8 +1,12 @@
 package com.vibbix.ballroom;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,10 +25,11 @@ import java.math.RoundingMode;
 import java.util.Currency;
 import java.util.Locale;
 
+import butterknife.BindArray;
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import butterknife.OnCheckedChanged;
 import butterknife.OnTextChanged;
 
 public class activity_main extends AppCompatActivity {
@@ -69,12 +74,35 @@ public class activity_main extends AppCompatActivity {
     String formattedResult;
     @BindView(R.id.transition_container)
     ViewGroup transition_container;
+    @BindString(R.string.link_github)
+    String githublink;
+    @BindString(R.string.link_project)
+    String projectlink;
+    @BindString(R.string.prefs_name)
+    String prefsName;
+    @BindString(R.string.prefs_area)
+    String prefsArea;
+    @BindString(R.string.prefs_cost)
+    String prefsCost;
+    @BindString(R.string.prefs_depth)
+    String prefsDepth;
+    @BindString(R.string.prefs_easy)
+    String prefsEasy;
+    @BindString(R.string.prefs_efficiency)
+    String prefsEfficiency;
+    @BindString(R.string.prefs_metric)
+    String prefsMetric;
+    @BindString(R.string.prefs_radius)
+    String prefsRadius;
+    @BindString(R.string.prefs_nightmode)
+    String prefsNightmode;
+    @BindArray(R.array.spinner_nightmode_items)
+    String[] nightmodeItems;
     //endregion
     private BallroomCalc ballroomCalc;
     private boolean wasMetric = false;
     private boolean wasEasy = false;
     private String currency;
-
     /**
      * Rounds a number to a certain amount of decimal points
      *
@@ -106,6 +134,7 @@ public class activity_main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        applyNightMode();
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
@@ -135,21 +164,17 @@ public class activity_main extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
-        updateArea();
-        updateDepth();
-        updateMoney();
-        updateRadius();
         skpacking.setMax(BallroomCalc.maxdensity.intValue());
-        skpacking.setProgress(skpacking.getProgress());
+        this.loadPreferences();
     }
 
-    @OnClick(R.id.switchMetric)
+    @OnCheckedChanged(R.id.switchMetric)
     void onMetricSwitch() {
         this.ballroomCalc.setIsMetric(swmetric.isChecked());
         this.updateEstimate();
     }
 
-    @OnClick(R.id.switchEasy)
+    @OnCheckedChanged(R.id.switchEasy)
     void onSwitchEasy() {
         this.ballroomCalc.setEasymode(easyswitch.isChecked());
         this.updateEstimate();
@@ -237,11 +262,93 @@ public class activity_main extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Intent intentabout = new Intent(this, activity_about.class);
-            startActivity(intentabout);
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                Intent intentabout = new Intent(this, activity_about.class);
+                startActivity(intentabout);
+                return true;
+            case R.id.action_project:
+                Intent projectIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(this.projectlink));
+                startActivity(projectIntent);
+                return true;
+            case R.id.action_github:
+                Intent githubIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(this.githublink));
+                startActivity(githubIntent);
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    private void savePreferences() {
+        SharedPreferences settings = getApplicationContext().getSharedPreferences(prefsName, 0);
+        SharedPreferences.Editor edit = settings.edit();
+        edit.putFloat(prefsArea, (float) this.ballroomCalc.getArea());
+        edit.putFloat(prefsDepth, (float) this.ballroomCalc.getDepth());
+        edit.putBoolean(prefsEasy, this.ballroomCalc.isEasyMode());
+        edit.putFloat(prefsEfficiency, (float) this.ballroomCalc.getEfficiency());
+        edit.putBoolean(prefsMetric, this.ballroomCalc.isMetric());
+        edit.putFloat(prefsCost, (float) this.ballroomCalc.getPrice());
+        //must commit, background thread will not save
+        edit.commit();
+    }
+
+    private void loadPreferences() {
+        SharedPreferences settings = getApplicationContext().getSharedPreferences(prefsName, 0);
+        this.ballroomCalc.setArea(settings.getFloat(prefsArea, 674.0f));
+        this.ballroomCalc.setDepth(settings.getFloat(prefsDepth, 2.0f));
+        this.ballroomCalc.setEasymode(settings.getBoolean(prefsEasy, false));
+        this.ballroomCalc.setEfficiency(settings.getFloat(prefsEfficiency, 64.0f));
+        boolean useImperial = Locale.getDefault().getCountry().equals("US")
+                || Locale.getDefault().getCountry().equals("LR")
+                || Locale.getDefault().getCountry().equals("MM");
+        this.ballroomCalc.setIsMetric(settings.getBoolean(prefsMetric, !useImperial));
+        this.ballroomCalc.setPrice(settings.getFloat(prefsCost, 0.20f));
+        this.refresh();
+    }
+
+    private void applyNightMode() {
+        SharedPreferences settings = getApplicationContext().getSharedPreferences(prefsName, 0);
+        String nightMode = settings.getString(prefsNightmode, "Auto");
+        if (nightMode.equals("Auto")) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+        } else if (nightMode.equals("Night")) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else if (nightMode.equals("Day")) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        }
+    }
+
+    private void refresh() {
+        updateArea();
+        updateDepth();
+        updateMoney();
+        updateRadius();
+        wasEasy = !easyswitch.isChecked();
+        wasMetric = !swmetric.isChecked();
+        onMetricSwitch();
+        onSwitchEasy();
+        skpacking.setProgress(skpacking.getProgress());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        this.savePreferences();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.savePreferences();
     }
 }
